944673ab112e3daec57e07e90ffa9ad92680cca1
kb0304 <karabedi0304@gmail.com>
2018-04-20 06:07:27 +0530

    Stable v2.0.0

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # meteor-pdfjs
+
+Meteor package for rendering PDF in client side
+
+`meteor add kb0304:pdfjs`
+
+Wrapper for `pdf.js` by Mozilla
+
+PDF.js website: *http://mozilla.github.io/pdf.js/*
---
+++ package.js
@@ -0,0 +1,13 @@
+Package.describe({
+  name: "kb0304:pdfjs",
+  summary: "Wrapper for serving pdfjs (client only)",
+  version: "2.0.0",
+  git: "https://github.com/kb0304/meteor-pdfjs",
+});
+
+Package.on_use(function (api) {
+  api.versionsFrom(['METEOR@0.9.3', 'METEOR@0.9.4', 'METEOR@1.0']);
+  api.add_files(['build/pdf.js'], 'client');
+  api.add_files(['build/pdf.worker.js'], 'client', {isAsset: true});
+  api.export('pdfjsLib'); 
+});
